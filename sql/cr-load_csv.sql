f2e9f5f68d988544459f7364c3dd81d87ed49d68
Vladimir Scherbo <vscherbo@gmail.com>
2021-07-28 18:44:30 +0300

    PG files

---
+++ sql/cr-load_csv.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE FUNCTION arc_energo.load_csv(arg_csv character varying)
+ RETURNS character varying
+ LANGUAGE plpgsql
+AS $function$
+begin
+    return 'finish';
+end;
+$function$
+;
+
